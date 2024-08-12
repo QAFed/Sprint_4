0c5c8d8dfeb39d5c67023190e268623a97e28556
Fedor <idolenkov@gmail.com>
2024-08-12 16:40:28 +0300

    add_new_book - tests complete

--- tests.py
+++ tests.py
@@ -20,7 +20,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -31,6 +31,35 @@ class TestBooksCollector:
         ('genre',['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']),
         ('genre_age_rating', ['Ужасы', 'Детективы'])
     ])
-    def test_class_init_data_correctly(self, param_name, param_value):
+    def test_class_init_data_correctly_created(self, param_name, param_value):
         collector = BooksCollector()
         assert getattr(collector, param_name) == param_value
+
+    @pytest.mark.parametrize('name', [
+        "",
+        "41сорокОдин41сорокОдин41сорокОдин41сорONE",
+        "50пятьДесятFIFTEEN5050пятьДесятFIFTEEN501111111111"
+    ])
+    def test_add_new_book_with_len_name_out_of_range_not_added(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name not in collector.get_books_genre()
+
+    @pytest.mark.parametrize('name', [
+        "1",
+        "40Cорок40!40Sorok40!40сорок40!40сорок40!",
+        "20dvadcatД20dvadcatД"
+    ])
+    def test_add_new_book_with_len_name_in_range_added(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name in collector.get_books_genre()
+
+    def test_add_new_book_then_book_already_exist_not_create(self):
+        collector = BooksCollector()
+        duble_book = "Duble_book"
+        genre_book = 'Мультфильмы'
+        collector.add_new_book(duble_book)
+        collector.set_book_genre(duble_book, genre_book)
+        collector.add_new_book(duble_book)
+        assert collector.get_books_genre()[duble_book] == genre_book
